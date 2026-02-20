4df98c65d5f6e6ac0cc7fcf6436cd5b5fb5b4fce
Om vataliya <omvataliya23@gmail.com>
2026-02-20 13:53:52 +0530

    Test-new-2

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-htmlskdalkjdajsdj
-kkdalkskdlj
-KNKSndklnasd
-kasdk
+// ERROR: Undefined variable accessed
+console.log(undefinedVariable);
+
+// ERROR: Syntax error - missing closing bracket
+const arr = [1, 2, 3;
+
+// ERROR: Type error - calling method on null
+null.toString();
+
+// ERROR: Reference error - using variable before declaration
+console.log(x);
+var x = 5;
